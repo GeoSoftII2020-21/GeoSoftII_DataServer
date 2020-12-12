4ba8e60b1328bb89b637336e4c882d9896c7a224
Leg-en <44808950+Leg-en@users.noreply.github.com>
2020-12-12 13:24:39 +0100

    Co-authored-by: Svub1 <s_buse01@uni-muenster.de>
    DataServer Endpunkte Gesetzt. Basic Funktionalität für Post funktion gesetzt.

---
+++ src/Server.py
@@ -0,0 +1,37 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route("/data", methods=["POST"])
+def postData():
+    """
+    Absprache mit Dev Team Erforderlich zum Daten Einladen
+    :return:
+    """
+    dataFromPost = request.get_json()
+    print(dataFromPost)
+    data = {
+    "code" : 200
+    }
+    return jsonify(data)
+
+@app.route("/data", methods=["GET"])
+def getData():
+    """
+    Absprache mit Dev Teams erforderlich
+    :returns:
+        jsonify(data): Datacube der vom Data Team geliefert wird
+    """
+    dataFromPost = request.get_json()
+    data = None
+    return jsonify(data)
+
+
+def main():
+    """
+    Startet den Server. Aktuell im Debug Modus und Reagiert auf alle eingehenden Anfragen auf Port 80.
+    """
+    app.run(debug=True, host="0.0.0.0", port=443)
+
+if __name__ == "__main__":
+    main()
