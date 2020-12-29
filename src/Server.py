373bfab18f0a7ded96b6bcf369c79eae87eeebe7
Leg-en <44808950+Leg-en@users.noreply.github.com>
2020-12-29 14:56:44 +0100

    Docker Integration Forgesetzt
    Co-authored-by: Svub1 <s_buse01@uni-muenster.de>

--- src/Server.py
+++ src/Server.py
@@ -1,6 +1,7 @@
-from flask import Flask, request, jsonify
-import requests
 import os
+
+import requests
+from flask import Flask, request, jsonify
 
 docker = False
 
@@ -8,21 +9,21 @@ app = Flask(__name__)
 
 job = {"status": "running"}
 
+
 @app.route("/doJob", methods=["POST"])
 def doJob():
     dataFromPost = request.get_json()
-    #Todo: Eval. Json
-    data = None #Todo: Json fordert Konkrete Daten an. API Aushandeln
-    r = requests.get("http://localhost:80/data", json=data)
-    #Todo: Funktions aufruf was daten bearbeitet
+    # Todo: Eval. Json
+    data = None  # Todo: Json fordert Konkrete Daten an. API Aushandeln
+    # Todo: Funktions aufruf was daten bearbeitet
 
     data = None
     return jsonify(data)
 
+
 @app.route("/jobStatus", methods=["GET"])
 def jobStatus():
     return jsonify(job)
-
 
 
 def main():
@@ -32,7 +33,12 @@ def main():
     global docker
     if os.environ.get("DOCKER") == "True":
         docker = True
-    app.run(debug=True, host="0.0.0.0", port=80)
+    if docker:
+        port = 80
+    else:
+        port = 443
+    app.run(debug=True, host="0.0.0.0", port=port)
+
 
 if __name__ == "__main__":
     main()
