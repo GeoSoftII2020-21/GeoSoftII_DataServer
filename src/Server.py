16cfc175316789257dd46a771bfc1fa215aab682
Sterthaus <44808950+Leg-en@users.noreply.github.com>
2021-01-18 17:26:42 +0100

    NDVI Prozess Integriert, Process Description angepasst, Data Server Integration Fortgesetzt
    Co-authored-by: Svub1 <s_buse01@uni-muenster.de>

--- src/Server.py
+++ src/Server.py
@@ -37,15 +37,22 @@ def loadData(dataFromPost, id):
                                               dataFromPost["arguments"]["timeframe"][2])
         Collections_Sentinel2_SST_Data.load_collection("SST",
                                                        [fromDate.year, toDate.year, os.path.join("/data/", str(id), "data/"), "cube"])
+        subid = uuid.uuid1()
+        fromFile = os.path.join("/data/", str(id), "data/sst.day.mean.cube.nc")
+        toFile = os.path.join("/data/", str(id), str(subid) + ".nc")
+        os.rename(fromFile, toFile)
+        shutil.rmtree("data/" + str(id) + "/data")
+        job["id"] = str(subid)
+        job["status"] = "done"
     elif dataFromPost["arguments"]["DataType"] == "Sentinel2":
-        print("Todo")
-    subid = uuid.uuid1()
-    fromFile = os.path.join("/data/", str(id), "data/sst.day.mean.cube.nc")
-    toFile = os.path.join("/data/", str(id), str(subid) + ".nc")
-    os.rename(fromFile, toFile)
-    shutil.rmtree("data/" + str(id) + "/data")
-    job["id"] = str(subid)
-    job["status"] = "done"
+        fromDate = datetime.datetime.strptime(dataFromPost["arguments"]["timeframe"][0],
+                                              dataFromPost["arguments"]["timeframe"][2])
+        toDate = datetime.datetime.strptime(dataFromPost["arguments"]["timeframe"][1],
+                                            dataFromPost["arguments"]["timeframe"][2])
+        fromDate = fromDate.strftime("%Y%m%d")
+        toDate = toDate.strftime("%Y%m%d")
+        params = [os.path.join("data/",str(id),"data"),(fromDate,toDate),(dataFromPost["arguments"]["cloudcoverage"][0],dataFromPost["arguments"]["cloudcoverage"][1]),dataFromPost["arguments"]["Login"][0],dataFromPost["arguments"]["Login"][1]]
+        Collections_Sentinel2_SST_Data.load_collection("Sentinel2",params)
 
 
 def main():
